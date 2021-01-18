f02dc5a45da182a5c35006973bd3661c1d83c4b0
Sahasil AK <76594303+SahasilAK@users.noreply.github.com>
2021-01-18 16:33:37 +0530

    Update scoreboard.py

--- scoreboard.py
+++ scoreboard.py
@@ -4,9 +4,10 @@ from turtle import Turtle
 
 
 # scoreboard constants
-FONT = ("Courier", 15, "normal")
+FONT = ("Courier", 12, "normal")
 SCORE_ALIGN = "center"
 SCORE_LOC = (-230, 260)
+HSCORE_LOC = (-220, 240)
 
 
 class Scoreboard(Turtle):
@@ -17,6 +18,8 @@ class Scoreboard(Turtle):
 		self.level = 0
 		self.penup()
 		self.hideturtle()
+		with open("data.txt") as data:
+			self.high_score = int(data.read())
 
 		self.update_scoreboard()
 
@@ -24,16 +27,28 @@ class Scoreboard(Turtle):
 		self.goto(SCORE_LOC)
 		self.write(f'Level : {self.level}',False , SCORE_ALIGN,FONT)
 
+		self.goto(HSCORE_LOC)
+		self.write(f'High Score : {self.high_score}',False , SCORE_ALIGN,FONT)
+
 	def increase_level(self):
 		self.level += 1
 		self.clear()
 		self.update_scoreboard()
+
 
 	def game_over(self):
 		self.goto(0,0)
 		self.write(f'Game Over',False , SCORE_ALIGN,FONT)
 		self.goto(0,-40)
 		self.write(f'You Reached Level : {self.level}',False , SCORE_ALIGN,FONT)
+		if self.level > self.high_score:
+			self.high_score = self.level
+
+			hscore = str(self.high_score)
+			with open("data.txt",mode = "w") as data:
+
+				data.write(hscore)
+
 
 
 
